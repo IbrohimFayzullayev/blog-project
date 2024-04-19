b1e77f1f93f64d9187256589ede7c7bdab7afc55
Ibrohim Fayzullayev <ibrohim_f01@mail.ru>
2024-04-19 11:54:23 +0500

    logo change

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,9 @@
-// import { Link } from "react-router-dom";
-// import Logo from "../images/logo.svg";
-// import Profile from "../images/profile.png";
-// import Avatar from "../images/ibrohim.jpg";
 import Me from "../images/me.jpg";
 import Projects from "../components/Projects";
 import Links from "../components/Links";
 import Experience from "../components/Experience";
-// import MePhoto from "../images/me-at-work.jpg";
+import { Link } from "react-router-dom";
+import Sticker from "../images/profile.jpg";
 
 const Home = () => {
   const articleStructuredData = {
@@ -35,15 +32,14 @@ const Home = () => {
         }}
       />
       <div className="bg-black w-full min-h-screen">
-        <div className="fixed flex justify-between items-center text-white text-2xl text-center py-4 px-4 w-full top-0">
-          <div></div>
-          {/* <Link to="/"> */}
-          {/* <img
-              src={Me}
-              alt="logo"
-              className="w-[50px] h-[50px] rounded-full"
-            /> */}
-          {/* </Link> */}
+        <div className="fixed flex justify-between items-center text-white text-2xl text-center py-4 px-4 w-full top-0 backdrop-blur-sm z-10">
+          <Link to="/">
+            <img
+              src={Sticker}
+              alt="Ibrohim Fayzullayev"
+              className="w-[53px] h-[50px] rounded-full"
+            />
+          </Link>
           <button className="bg-blue-700 text-white font-normal text-base rounded-lg px-4 py-2 hover:bg-blue-600 transition-all duration-150">
             CV
           </button>
